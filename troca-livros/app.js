8452bc5acad4f3a1a62dd24a130b09ee92a139cf
victorleonardo <vlmas@discente.ifpe.edu.br>
2025-02-11 20:02:28 -0300

    Corrigindo estrutura do projeto

--- troca-livros/app.js
+++ troca-livros/app.js
@@ -30,3 +30,9 @@ const bookRoutes = require("./routes/book");
 app.use(bookRoutes);
 
 require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
